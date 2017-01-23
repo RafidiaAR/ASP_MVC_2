2ca9632ac138a338fbdaf576bc80e3e3bfec497f
Rafidia <Rafidia@RAFID>
2017-01-23 07:51:13 +0700

    Add project files.

---
+++ ASP_MVC_2/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace ASP_MVC_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome() {
+            return View();
+        }
+    }
+}
